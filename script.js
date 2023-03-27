b491b1a290dda3153541e063faa3e5d6143e3f03
sahilpatel0oo7 <109283616+sahilpatel0oo7@users.noreply.github.com>
2023-03-28 00:18:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+flash = 1
+
+function b1() {
+    
+    if (flash == 1) { 
+        document.getElementById("b1").value = "X";
+        flash = 0
+
+    }
+
+    else {document.getElementById("b1").value = "0";
+    flash = 1}
+
+}
+function b2() {
+    
+    if (flash == 1) { 
+        document.getElementById("b2").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b2").value = "0";
+    flash = 1}
+
+}
+function b3() {
+    
+    if (flash == 1) { 
+        document.getElementById("b3").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b3").value = "0";
+    flash = 1}
+
+}
+function b4() {
+    
+    if (flash == 1) { 
+        document.getElementById("b4").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b4").value = "0";
+     
+    flash = 1}
+
+}
+function b5() {
+    
+    if (flash == 1) { 
+        document.getElementById("b5").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b5").value = "0";
+    flash = 1}
+
+}
+function b6() {
+    
+    if (flash == 1) { 
+        document.getElementById("b6").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b6").value = "0";
+    flash = 1}
+
+}
+function b7() {
+    
+    if (flash == 1) { 
+        document.getElementById("b7").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b7").value = "0";
+    flash = 1}
+
+}
+function b8() {
+    
+    if (flash == 1) { 
+        document.getElementById("b8").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b8").value = "0";
+    flash = 1}
+
+}
+function b9() {
+    
+    if (flash == 1) { 
+        document.getElementById("b9").value = "X"
+     flash = 0}
+
+    else {document.getElementById("b9").value = "0";
+    flash = 1}
+
+}
+
+function refresh(){
+    document.getElementById("b1").value = "";
+    document.getElementById("b2").value = "";
+    document.getElementById("b3").value = "";
+    document.getElementById("b4").value = "";
+    document.getElementById("b5").value = "";
+    document.getElementById("b6").value = "";
+    document.getElementById("b7").value = "";
+    document.getElementById("b8").value = "";
+    document.getElementById("b9").value = "";
+   
+}
